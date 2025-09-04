16f592c24250d0f27bad0d42c4f4f899b88446c0
s3unghyeon <user@userui-MacBookPro-2.local>
2025-09-04 10:38:29 +0900

    modify setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -19,8 +19,8 @@ RESET='\033[0m'
 
 # Constants
 SCRIPT_NAME="$(basename "$0")"
-LOG_FILE="/var/log/boundless_prover_setup.log"
-ERROR_LOG="/var/log/boundless_prover_error.log"
+LOG_FILE="/home/ubuntu/log/boundless_prover_setup.log"
+ERROR_LOG="/home/ubuntu/log/boundless_prover_error.log"
 INSTALL_DIR="$HOME/boundless"
 COMPOSE_FILE="$INSTALL_DIR/compose.yml"
 BROKER_CONFIG="$INSTALL_DIR/broker.toml"
