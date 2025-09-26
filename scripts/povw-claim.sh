709e3267c1abdedae518872b30aa85d2fc0ac57d
s3unghyeon <user@userui-MacBookPro-2.local>
2025-09-26 21:52:44 +0900

    add povw scripts

---
+++ scripts/povw-claim.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+boundless povw claim --log-id 0x07F67C58E07DB79fA40a15e600A96BD5220AE0D3 --beacon-api-url https://alien-warmhearted-waterfall.quiknode.pro/9c4ca8a4adfdde507fc83b1f2ffa932f441ace5c/ --rpc-url https://alien-warmhearted-waterfall.quiknode.pro/9c4ca8a4adfdde507fc83b1f2ffa932f441ace5c/
---
+++ scripts/povw-submit.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+boundless povw prepare --state /home/ubuntu/.state/state.bin --from-bento
+boundless povw submit --state /home/ubuntu/.state/state.bin --value-recipient 0x1c11c45d6f5642c334Ab5a552424DF2BC943Eb05 --rpc-url https://alien-warmhearted-waterfall.quiknode.pro/9c4ca8a4adfdde507fc83b1f2ffa932f441ace5c/
